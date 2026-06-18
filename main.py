0db96f3798c1415746b8877e3f4965f188110d30
Joao Victor Mendes de Siqueira <joaosiqueira.2005@alunos.utfpr.edu.br>
2026-06-18 11:23:09 -0300

    Projeto funcionando para mysql e postgreSQL

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from gui.MainWindow import MainWindow
 
 
 def main():
+    #inicia o lop da gui
     app = QApplication(sys.argv)
 
-    window = MainWindow()
-    window.show()
+    window = MainWindow()#inicia a janela
+    window.show()#mosta a janela
 
     sys.exit(app.exec())
 
